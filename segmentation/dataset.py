2e2029b0972d334b9c56ba132005f094ea26820d
Malong <1450271751@qq.com>
2018-08-16 19:42:08 +0800

    add segment laser and re-implement the segmentation dataset

--- segmentation/dataset.py
+++ segmentation/dataset.py
@@ -1,5 +1,10 @@
+import os
+from pathlib import Path
+
 import cv2
 from PIL import Image
+
+import numpy as np
 
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -7,7 +12,91 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as F
 
-__all__ = ["SegData"]
+from transform import _ToTensor
+
+   
+class LaserDataset(Dataset):
+    def __init__(self, datapath, class_nums, train=True, transform=None):
+        """datapath should have two folders when train
+        is True, and one folder when train=False.
+        """
+        self.file_path = datapath
+        self.transform = transform
+        self.class_nums = class_nums
+        self.train = train
+        if self.train:
+            self.x_fns, self.y_fns = self.get_fns(self.file_path, train=True)
+        else:
+            self.x_fns = self.get_fns(self.file_path, train=False)
+        img_test = cv2.imread(self.x_fns[0])
+        self.init_grid(img_test.shape[0], img_test.shape[1])
+        
+    def __getitem__(self, index):
+        if self.train:
+            # x_fn, y_fn = self.x_fns[index], self.y_fns[index]
+            x_fn, y_fn = self.x_fns[index], self.y_fns[index]
+            x, y = self.get_data_pair(x_fn, y_fn)
+            if self.transform is not None:
+                x, y = self.transform(x, y)
+            else:
+                x, y = _ToTensor()(x), _ToTensor()(y)
+            return x, y
+        else:
+            # fn = self.x_fns[index]
+            fn = self.x_fns[index]
+            x = Image.open(fn)
+            if self.transform is not None:                
+                x = self.transform(x)
+            else:
+                x = _ToTensor()(x)
+            return x
+        
+    def __len__(self):
+        return len(self.y_fns)
+
+    def __repr__(self):
+        fmt_str = 'line laser dataset for segmentation'
+        return fm_str
+
+    def get_data_pair(self, x_fn, y_fn):
+        x = Image.open(x_fn)
+        y = self.get_label(y_fn)
+        return x, y
+
+    def get_label(self, filepath, is_one_hot=False):
+        label = cv2.imread(filepath)[:, :, 2]
+        if is_one_hot:
+            one_hot = np.zeros((self.class_nums, label.shape[0], label.shape[1]), dtype=np.uint8)        
+            one_hot[label.reshape(-1), self._hh, self._ww] = 255
+            return one_hot
+        else:
+            label[label==1] = 255
+            return label.reshape(1, label.shape[0], label.shape[1])
+
+    def init_grid(self, shape0, shape1):
+        h = np.arange(0, shape0)
+        w = np.arange(0, shape1)
+        ww, hh = np.meshgrid(w, h)
+
+        self._ww = ww.reshape(-1)
+        self._hh = hh.reshape(-1)
+
+    def get_fns(self, filepath, train=True):
+        filepath = Path(filepath)
+        if train:
+            x_path = Path('data')
+            y_path = Path('label')
+            x_fns = os.listdir(str(filepath/x_path))
+            y_fns = os.listdir(str(filepath/y_path))
+            x_fns = [str(filepath/x_path/Path(t)) for t in x_fns]
+            y_fns = [str(filepath/y_path/Path(t)) for t in y_fns]
+            return x_fns, y_fns
+        else:
+            x_path = Path('data')
+            x_fns = os.listdir(str(filepath/x_path))
+            x_fns = [str(filepath/x_path/Path(t)) for t in x_fns]
+            return x_fns 
+
 
 class SegData(Dataset):
     def __init__(self, dataset, train=True, transform=None):
@@ -30,7 +119,6 @@ class SegData(Dataset):
         
         if self.transform is not None:
             img, target = self.transform(img, target)
-
         return img, target
 
     def __len__(self):
@@ -39,6 +127,14 @@ class SegData(Dataset):
         else:
             return len(self.test_data)
 
+    def __repr__(self):
+        fmt_str = 'undersea segmentation'
+        return fmt_str
+
+class Seg_Data_Beta(LaserDataset):
+    def __init__(self, datapath, class_nums, train=True, transform=None):
+        super().__init__(datapath, class_nums, train=True, transform=None)
+    
     def __repr__(self):
         fmt_str = 'undersea segmentation'
         return fmt_str
--- segmentation/train.py
+++ segmentation/train.py
@@ -1,52 +1,243 @@
-from pathlib import Path
+import torch
+from torch import nn
 
-from dataset import *
-from transform import *
-from utils import *
+class AlbuNet_LSTM_Loss:
+    def __init__(self, k, pos_neg_prop, no_ground_class_nums):
+#     def __init__(self, no_ground_class_nums):
+        self.pos_neg_prop = pos_neg_prop
+        self.k = k
+        self.class_nums = no_ground_class_nums
+        self.score_loss = nn.BCEWithLogitsLoss()
+        self.class_loss = nn.BCEWithLogitsLoss()
+        # self.bbox_loss = nn.MSELoss()
+        self.bbox_loss = nn.SmoothL1Loss()
+        self.mask_loss = nn.BCEWithLogitsLoss()
+        
+    def __call__(self, net_output, label):
+        """
+        net_output: BSx(40x40xT)x85
+        TODO: NOTE that the loss have an another dim 'bs'.
+        torch.Size([1, 8000, 85])
+        -----------
+        torch.Size([1, 8000])
+        torch.Size([1, 8000, 4])
+        torch.Size([1, 8000])
+        torch.Size([1, 8000])
+        torch.Size([1, 8000])
+        torch.Size([1, 128, 1, 28, 28])
+        """
 
-MODEL_INPUT_SIZE = 512
-RAW_IMAGE_SIZE = 1080
+        BS, H, W, T, C = net_output.shape
+        net_output = net_output.reshape(BS, H*W*T, -1)
+        out_scores, out_bbox, train_index, positive_index, negative_index, out_target = label
+        
+        net_output = net_output.reshape(-1, net_output.shape[-1])
+        out_scores = out_scores.reshape(-1, 1)
+        out_bbox = out_bbox.reshape(-1, 4)
+        train_index = train_index.reshape(-1)
+        negative_index = negative_index.reshape(-1)
+        positive_index = positive_index.reshape(-1)
+        
+        negative_index_copy1 = negative_index.reshape(BS, H, W, T).clone()
+        negative_index_copy1[:, :, :, 0] = 0
+        
+        negative_num = torch.sum(negative_index)
+        positive_num = torch.sum(positive_index)
+        negative_index_copy = negative_index.clone()
+        choice = torch.randperm(negative_num)
+        if net_output.is_cuda:
+            choice = choice.cuda()
+        negative_index_copy[negative_index] = choice < max(positive_num * 10, 10)
+        negative_index = negative_index_copy | negative_index_copy1.reshape(-1)
+                    
+        train_index = positive_index |  negative_index
+        
+        # scores
+        scores_label = (out_scores[train_index, 0] > 0).type(torch.float)
+        # weight = self.k*self.pos_neg_prop*positive_index.type(torch.float) + \
+        #             self.k*(1-self.pos_neg_prop)*negative_index.type(torch.float)
+        # if net_output.is_cuda:
+        #     weight = weight.cuda()
+        # self.score_loss = nn.BCEWithLogitsLoss(weight[train_index])
+        score_loss = self.score_loss(net_output[train_index, 0], scores_label)
+        
+        # bbox
+        if torch.sum(positive_index) == 0:
+            class_loss = torch.zeros([1], dtype=torch.float)
+            bbox_loss = torch.zeros([1], dtype=torch.float)
+            if net_output.is_cuda:
+                class_loss = class_loss.cuda()
+                bbox_loss = bbox_loss.cuda()
+                return class_loss + bbox_loss + score_loss, [class_loss.item(), bbox_loss.item(), score_loss.item()]
+        else:
+            # bbox_xy = torch.nn.functional.tanh(net_output[positive_index, -4:-2])
+            bbox_xy = torch.tanh(net_output[positive_index, -4:-2])
+            # bbox_wh = torch.exp(torch.nn.functional.sigmoid(net_output[positive_index, -2:]))
+            # bbox_wh = torch.log(torch.nn.functional.sigmoid(net_output[positive_index, -2:]))
+            # bbox_wh = torch.nn.functional.sigmoid(net_output[positive_index, -2:])
+            bbox_wh = torch.sigmoid(net_output[positive_index, -2:])
+            # network_out_bbox = torch.cat([bbox_xy, bbox_wh], dim=1)
+            # bbox_loss = self.bbox_loss(network_out_bbox, out_bbox[positive_index, :])*4
+            # bbox_loss = self.bbox_loss(network_out_bbox, torch.log(out_bbox[positive_index, :]))*4
+            bbox_xy_loss = self.bbox_loss(bbox_xy, out_bbox[positive_index, -4:-2])
+            # print(torch.max(net_output[positive_index, -2:]), torch.min(net_output[positive_index, -2:]))
+            # print(torch.max(out_bbox[positive_index, -2:]), torch.min(out_bbox[positive_index, -2:]))
+            bbox_wh_loss = self.bbox_loss(torch.exp(bbox_wh), torch.exp(out_bbox[positive_index, -2:]))
+            bbox_loss = bbox_xy_loss + bbox_wh_loss
 
-data_read, label_read = get_data(Path('./undersea'))
-samples = len(data_read)
-choice = list(np.random.permutation(samples))
-data_random = [data_read[x] for x in choice]
-label_random = [label_read[x] for x in choice]
+            # classify
+            # samples = out_scores[positive_index, 0].shape[0]
+            samples = torch.sum(positive_index).item()
+            if net_output.is_cuda:
+                class_one_hot_label = torch.zeros(samples, self.class_nums+1).type(torch.cuda.FloatTensor)
+                one_hot_index = out_scores[positive_index, 0].type(torch.cuda.LongTensor).reshape(-1, 1)
+            else:
+                class_one_hot_label = torch.zeros(samples, self.class_nums+1)
+                one_hot_index = out_scores[positive_index, 0].type(torch.LongTensor).reshape(-1, 1)
+            class_one_hot_label = class_one_hot_label.scatter_(1, one_hot_index, 1)
+            class_loss = self.class_loss(net_output[positive_index, 1:-4], class_one_hot_label[:, 1:])
+        return class_loss + bbox_loss + score_loss, [class_loss.item(), bbox_loss.item(), score_loss.item()]
 
-k_split = 0.8
-train_samples = int(np.floor(k_split*samples))
+def train_AlbuNet_LSTM(model, loss_fn, optimizer, loader_train, log, **config):
+    epochs = config.get('epochs', 1)
+    is_cuda_type = config.get('is_cuda_type', False)
+    print_every = config.get('print_log_every_step', 30)
+    learning_rate = config.get('learning_rate', 1e-4)
+    decay_lr_time = config.get('decay_learning_rate_at_epcho', [None])
+    print(epochs)
+    
+    for p in optimizer.param_groups:
+        p['lr'] = learning_rate
+    print('learning_rate: ', learning_rate)
+    
+    model.train()
+    
+    step = 0
+    for epoch in range(epochs):
+        print('Starting epoch %d / %d' % (epoch + 1, epochs))
+        for x, y in loader_train:           
+            if is_cuda_type:
+                x_train = x.type(torch.cuda.FloatTensor)
+                out_scores, out_bbox, train_index, positive_index, negative_index, out_target = y
+                y_train = out_scores.type(torch.cuda.FloatTensor), \
+                            out_bbox.type(torch.cuda.FloatTensor), \
+                            train_index.type(torch.cuda.ByteTensor), \
+                            positive_index.type(torch.cuda.ByteTensor), \
+                            negative_index.type(torch.cuda.ByteTensor), \
+                            out_target.type(torch.cuda.FloatTensor)
+            else:
+                x_train = x.type(torch.FloatTensor)
+                y_train = y
+            
+            hidden = model.init_rnn_state(config['batch_size']*config['grid_height']*config['grid_width'])
+            if is_cuda_type:
+                hx, cx = hidden
+                hidden = hx.type(torch.cuda.FloatTensor), cx.type(torch.cuda.FloatTensor)
+            
+            net_output = model(x_train, hidden)
+          
+            loss, [class_loss, bbox_loss, score_loss] = loss_fn(net_output, y_train)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
+            optimizer.step()
 
-train_set = (data_random[0:train_samples], label_random[0:train_samples])
-test_set = (data_random[train_samples:], label_random[train_samples:])
+            log['class_loss'].append(class_loss)
+            log['bbox_loss'].append(bbox_loss)
+            log['score_loss'].append(score_loss)
+            
+            if (step+1) % print_every == 1:
+                lr = optimizer.param_groups[0]['lr']
+                print('lr=%.9f, t = %d, class loss = %.9f, bbox_loss = %.9f,  score loss = %.9f' % (lr, step + 1, 
+                                                    class_loss, bbox_loss, score_loss))
+#                 print('lr=%.9f, t = %d, class loss = %.9f, bbox_loss = %.9f,  score loss = %.9f' % (lr, step + 1, 
+#                                                     0, 0, 0))
 
-transform_train = _Compose([
-                        _RandomApply([
-                            _Resize(scale_range=(0.5, 1.5), raw=RAW_IMAGE_SIZE),
-                        ], p=0.5),
-                        _RandomCrop(MODEL_INPUT_SIZE, MODEL_INPUT_SIZE),
-                        _RandomRotation(0.2, 360),
-                        _ColorJitter(brightness=0, contrast=0, saturation=0, hue=0),
-                        _RandomHorizontalFlip(0.5),
-                        _RandomVerticalFlip(0.5),
-                        _RandomGrayscale(0.01),
-                        _Lambda(lambda x, y: (_ToTensor()(x), _ToTensor()(y)))
-                ])
+            step = step + 1
+            # if step == epochs:
+            #     return
+            
+        if epoch in decay_lr_time:
+            for p in optimizer.param_groups:
+                p['lr'] = p['lr'] * 0.8
+            print('learning rate', p['lr'])
 
-train_torch = SegData(train_set, train=True, transform=transform_train)
-valid_torch = SegData(test_set, train=False, transform=None)
+import matplotlib.pyplot as plt
 
-train_loader = torch.utils.data.DataLoader(train_torch,
-                                          batch_size=4,
-                                          shuffle=True,
-                                          num_workers=8)
+class ShallowNet_Loss:
+    def __init__(self):
+        self.loss_fn = None
 
-valid_loader = torch.utils.data.DataLoader(valid_torch,
-                                          batch_size=4,
-                                          shuffle=False,
-                                          num_workers=1)
+    def __call__(self, logits, y):
+        # y: bs, 1, H, W
+        BS, C, H, W = y.shape
+        y = y.reshape(-1)
+        logits = logits.reshape(-1)
 
-epchos = 800
-for epcho in range(epchos):
-    for idx, (x, y) in enumerate(train_loader):
-        len(x)
-    print(epcho)
+        predict = logits > 0
+        label = (y>0.3)
+        false_positive = (predict==True)&(label==False)
+        print(torch.sum(false_positive).item())
+
+        p_weight = torch.Tensor([600])
+        if y.is_cuda:
+            p_weight = p_weight.cuda()
+        
+        weight = torch.ones_like(logits)
+        if y.is_cuda:
+            weight = weight.cuda()
+        weight[false_positive] = 600
+
+        self.loss_fn = nn.BCEWithLogitsLoss(weight=weight, pos_weight=p_weight)
+        return self.loss_fn(logits, y.type(torch.float))
+
+
+def train_ShallowNet(model, loss_fn, optimizer, loader_train, log, **config):
+    epochs = config.get('epochs', 1)
+    is_cuda_type = config.get('is_cuda_type', False)
+    print_every = config.get('print_log_every_step', 30)
+    learning_rate = config.get('learning_rate', 1e-4)
+    decay_lr_time = config.get('decay_learning_rate_at_epcho', [None])
+    
+    for p in optimizer.param_groups:
+        p['lr'] = learning_rate
+    print('learning_rate: ', learning_rate)
+    
+    model.train()
+    
+    step = 0
+    for epoch in range(epochs):
+        print('Starting epoch %d / %d' % (epoch + 1, epochs))
+        for x, y in loader_train:           
+            if is_cuda_type:
+                x_train = x.type(torch.cuda.FloatTensor)
+                y_train = y.type(torch.cuda.FloatTensor)
+            else:
+                x_train = x.type(torch.FloatTensor)
+                y_train = y
+                       
+            net_output = model(x_train)
+            # print(net_output.max().item(), net_output.min().item())
+            loss = loss_fn(net_output, y_train)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
+            optimizer.step()
+
+            log['loss'].append(loss.item())
+
+            if (step+1) % print_every == 1:
+                lr = optimizer.param_groups[0]['lr']
+                print('lr=%.9f, t = %d, loss = %.9f,' % (lr, step + 1, loss))
+
+            step = step + 1
+            # if step == epochs:
+            #     return
+            
+        if epoch in decay_lr_time:
+            for p in optimizer.param_groups:
+                p['lr'] = p['lr'] * 0.1
+            print('learning rate', p['lr'])
+
