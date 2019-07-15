56ad41e025fca3e1e2867b696a44f0c3384d76c4
MaXinglong <mxl12138@hrbeu.edu.cn>
2019-07-15 10:58:30 +0800

    add some algorithm implementation ipynb(deecamp2019)

---
+++ alg/permutation_combination.py
@@ -0,0 +1,18 @@
+def permutation(index_list, num):
+    choice = []
+    
+    index_choice = index
+    
+    if num == 1:
+        for val in index_list:
+            choice += [val]
+        return choice
+    
+    for i in range()
+        choice += permutation(index_list, num)
+    num = num - 1
+    
+    return choice
+
+
+permutation([1, 2, 3], 1)
