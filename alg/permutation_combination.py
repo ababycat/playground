d3008b6b89263dd700267890851bfcded6ee397d
MaXinglong <mxl12138@hrbeu.edu.cn>
2019-07-16 10:30:40 +0800

    add permutation and combination

--- alg/permutation_combination.py
+++ alg/permutation_combination.py
@@ -1,18 +1,38 @@
-def permutation(index_list, num):
-    choice = []
+def permutation(array):
+    if len(array) == 1:
+        return [array]
+    output = []
+    for i in range(len(array)):
+        if i == len(array) - 1:
+            remain = array[0:i]
+        else:
+            remain = array[0:i] + array[i+1:]
+        for back in permutation(remain):
+            output.append([array[i]] + back)
+    return output
     
-    index_choice = index
     
-    if num == 1:
-        for val in index_list:
-            choice += [val]
-        return choice
+def combine(array, n):
+    if len(array) == n:
+        return [array]
+    elif n == 0:
+        return []
+    output = []
+    for i in range(len(array) - n + 1):
+        remain = array[i+1:]
+        if n - 1 == 0:
+            output.append([array[i]])
+        else:
+            for r in combine(remain, n-1):
+                output.append([array[i]] + r)
+    return output
     
-    for i in range()
-        choice += permutation(index_list, num)
-    num = num - 1
     
-    return choice
+array = [1, 2, 3, 4]
+for line in permutation(array):
+    print(line)
 
 
-permutation([1, 2, 3], 1)
+A = [2, 3, 5, 2]
+for line in combine(A, 2):
+    print(line)
