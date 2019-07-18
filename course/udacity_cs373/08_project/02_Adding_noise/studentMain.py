d4f12f17b68ba4313bd44a3f565ab0e0e86aca24
MaXinglong <mxl12138@hrbeu.edu.cn>
2019-07-18 14:07:08 +0800

    udacity cs373 ai for robot project

---
+++ course/udacity_cs373/08_project/02_Adding_noise/studentMain.py
@@ -0,0 +1,220 @@
+# ----------
+# Part Two
+#
+# Now we'll make the scenario a bit more realistic. Now Traxbot's
+# sensor measurements are a bit noisy (though its motions are still
+# completetly noise-free and it still moves in an almost-circle).
+# You'll have to write a function that takes as input the next
+# noisy (x, y) sensor measurement and outputs the best guess 
+# for the robot's next position.
+#
+# ----------
+# YOUR JOB
+#
+# Complete the function estimate_next_pos. You will be considered 
+# correct if your estimate is within 0.01 stepsizes of Traxbot's next
+# true position. 
+#
+# ----------
+# GRADING
+# 
+# We will make repeated calls to your estimate_next_pos function. After
+# each call, we will compare your estimated position to the robot's true
+# position. As soon as you are within 0.01 stepsizes of the true position,
+# you will be marked correct and we will tell you how many steps it took
+# before your function successfully located the target bot.
+
+# These import steps give you access to libraries which you may (or may
+# not) want to use.
+from robot import *  # Check the robot.py tab to see how this works.
+from math import *
+from matrix import * # Check the matrix.py tab to see how this works.
+import random
+
+# This is the function you have to write. Note that measurement is a 
+# single (x, y) point. This function will have to be called multiple
+# times before you have enough information to accurately predict the
+# next position. The OTHER variable that your function returns will be 
+# passed back to your function the next time it is called. You can use
+# this to keep track of important information over time.
+from copy import deepcopy
+def measure_prob(x1, x2, noise_sigma):
+    # compute errors
+    error = 1.0
+    for i in range(len(x1)):
+        error_i = abs(x1[i] - x2[i])
+        # update Gaussian
+        error *= (exp(- (error_i ** 2) / (noise_sigma ** 2) / 2.0) /  
+                    sqrt(2.0 * pi * (noise_sigma ** 2)))
+    return error
+
+def estimate_next_pos(measurement, OTHER = None):
+    """Estimate the next (x, y) position of the wandering Traxbot
+    based on noisy (x, y) measurements."""
+
+    # You must return xy_estimate (x, y), and OTHER (even if it is None) 
+    # in this order for grading purposes.
+    P_LENGTH = 500
+    if not OTHER:
+        OTHER = {}
+        OTHER["p_list"] = []
+        for i in range(P_LENGTH):
+            r = robot(random.gauss(measurement[0], measurement_noise*10),
+                    random.gauss(measurement[1], measurement_noise*10),
+                    heading=(random.random()-0.5) * 2 * pi,
+                    turning=(random.random()-0.5) * pi,
+                    distance=random.gauss(1.5, 2))
+            OTHER["p_list"].append(r)
+
+    # prediction
+    for p in OTHER["p_list"]:
+        p.turning += random.gauss(0, pi/20)
+        p.distance += random.gauss(0, 0.01)
+        p.move(p.turning, p.distance)
+
+    # measure
+    w = []
+    for p in OTHER["p_list"]:
+        w.append(
+            measure_prob((p.x, p.y), 
+                         (measurement[0], measurement[1]), 
+                         sqrt(measurement_noise)))
+
+    # resampling
+    p3 = []
+    index = int(random.random() * P_LENGTH)
+    beta = 0.0
+    mw = max(w)
+
+    for i in range(P_LENGTH):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % P_LENGTH
+        p = deepcopy(OTHER["p_list"][index])
+        p3.append(p)
+    OTHER["p_list"] = p3
+
+    # prediction
+    p_list = deepcopy(OTHER["p_list"])
+    for p in p_list:
+        p.move(p.turning, p.distance)
+
+    # estimation
+    xy_estimate = (sum([x.x for x in p_list]) / P_LENGTH,
+                   sum([x.y for x in p_list]) / P_LENGTH)
+    # xy_estimate = (sum([x.x for x in OTHER["p_list"]]) / P_LENGTH, 
+    #                sum([x.y for x in OTHER["p_list"]]) / P_LENGTH)
+    return xy_estimate, OTHER
+
+# A helper function you may find useful.
+def distance_between(point1, point2):
+    """Computes distance between point1 and point2. Points are (x, y) pairs."""
+    x1, y1 = point1
+    x2, y2 = point2
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+# This is here to give you a sense for how we will be running and grading
+# your code. Note that the OTHER variable allows you to store any 
+# information that you want. 
+def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    while not localized and ctr <= 1000:
+        ctr += 1
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        if error <= distance_tolerance:
+            print "You got it right! It took you ", ctr, " steps to localize."
+            localized = True
+        if ctr == 1000:
+            print "Sorry, it took you too many steps to localize the target."
+    return localized
+
+
+def demo_grading_show(estimate_next_pos_fcn, target_bot, OTHER = None):
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    #For Visualization
+    import turtle    #You need to run this locally to use the turtle module
+    window = turtle.Screen()
+    window.bgcolor('white')
+    size_multiplier= 25.0  #change Size of animation
+    broken_robot = turtle.Turtle()
+    broken_robot.shape('turtle')
+    broken_robot.color('green')
+    broken_robot.resizemode('user')
+    broken_robot.shapesize(0.1, 0.1, 0.1)
+    measured_broken_robot = turtle.Turtle()
+    measured_broken_robot.shape('circle')
+    measured_broken_robot.color('red')
+    measured_broken_robot.resizemode('user')
+    measured_broken_robot.shapesize(0.1, 0.1, 0.1)
+    prediction = turtle.Turtle()
+    prediction.shape('arrow')
+    prediction.color('blue')
+    prediction.resizemode('user')
+    prediction.shapesize(0.1, 0.1, 0.1)
+    prediction.penup()
+    broken_robot.penup()
+    measured_broken_robot.penup()
+    #End of Visualization
+    while not localized and ctr <= 1000:
+        ctr += 1
+        # print('------step=>', ctr)
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        if error <= distance_tolerance:
+            print "You got it right! It took you ", ctr, " steps to localize."
+            localized = True
+        if ctr == 1000:
+            print "Sorry, it took you too many steps to localize the target."
+        #More Visualization
+        measured_broken_robot.setheading(target_bot.heading*180/pi)
+        measured_broken_robot.goto(measurement[0]*size_multiplier, measurement[1]*size_multiplier-200)
+        measured_broken_robot.stamp()
+        broken_robot.setheading(target_bot.heading*180/pi)
+        broken_robot.goto(target_bot.x*size_multiplier, target_bot.y*size_multiplier-200)
+        broken_robot.stamp()
+        prediction.setheading(target_bot.heading*180/pi)
+        prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
+        prediction.stamp()
+        #End of Visualization
+    return localized
+
+# This is a demo for what a strategy could look like. This one isn't very good.
+def naive_next_pos(measurement, OTHER = None):
+    """This strategy records the first reported position of the target and
+    assumes that eventually the target bot will eventually return to that 
+    position, so it always guesses that the first position will be the next."""
+    if not OTHER: # this is the first measurement
+        OTHER = measurement
+    xy_estimate = OTHER 
+    return xy_estimate, OTHER
+
+# random.seed(1)
+
+# This is how we create a target bot. Check the robot.py file to understand
+# How the robot class behaves.
+test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
+measurement_noise = 0.05 * test_target.distance
+test_target.set_noise(0.0, 0.0, measurement_noise)
+
+# demo_grading(naive_next_pos, test_target)
+demo_grading(estimate_next_pos, test_target)
+# demo_grading_show(estimate_next_pos, test_target)
+
